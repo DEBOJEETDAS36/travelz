6f4cdb8d22f5f079b7db8ae579a8ba919f8b4038
Shuvadeep Mandal <shuvadeepmandal5@gmail.com>
2025-03-18 21:24:02 +0530

    CLS upgrade

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,6 +1,6 @@
 import { Loader2 } from "lucide-react";
 const Loading = () => {
-  return <main className="wrapper min-h-[50v] fl_center">
+  return <main className="wrapper min-h-[50vh] fl_center">
     <Loader2 className="animate-spin text-primary size-8"/>
   </main>;
 };
